093e9d9f28b2e1d4926a4eb180da3838f0fe4d13
Hiduino Domingos <hvduino@gmail.com>
2021-03-25 08:11:15 -0300

    ♻️ Add Booking factory

--- lib/support/factory.ex
+++ lib/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Flightex.Factory do
   use ExMachina
 
+  alias Flightex.Bookings.Booking
   alias Flightex.Users.User
 
   def user_factory do
@@ -9,6 +10,16 @@ defmodule Flightex.Factory do
       name: "Hiduino",
       cpf: "123.456.789-00",
       email: "hiduino@mail.com"
+    }
+  end
+
+  def booking_factory do
+    %Booking{
+      id: UUID.uuid4(),
+      cidade_destino: "New York",
+      cidade_origem: "Luanda",
+      data_completa: ~N[2020-02-22 18:09:30],
+      id_usuario: "b5d6bd96-76de-4ced-bce1-535d6d558c49"
     }
   end
 end
